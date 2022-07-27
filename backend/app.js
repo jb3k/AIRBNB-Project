7c765479079186784c8a6ace3551b311876ae168
JB Kam <jb3.kam@gmail.com>
2022-07-27 13:05:01 -0700

    phase2 done

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 
 const { environment } = require('./config');
@@ -44,5 +45,45 @@ const routes = require('./routes');
 
 
 app.use(routes);
+
+//phase2 part 1
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+
+//phase 2 part 2
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+
+//phase 2 part 3
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
+
+
+
+
+
 
 module.exports = app;
