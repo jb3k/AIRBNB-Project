928ab74ef19c05365cc208b1dcb8684633a941c1
JB Kam <jb3.kam@gmail.com>
2022-08-01 16:32:48 -0700

    updated nested objects in the res.json in routes api

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     previewImage: {
-      type: DataTypes.BOOALEAN,
+      type: DataTypes.BOOLEAN,
       allowNull: false
     },
     spotId: {
