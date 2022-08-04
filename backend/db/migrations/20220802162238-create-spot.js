c2b9e6f07477d71d8a9bb7fb5ac7881d08aa85b0
JB Kam <jb3.kam@gmail.com>
2022-08-04 08:49:31 -0700

    things were breaking when i removed allow null false in my migrations so adding back.

--- backend/db/migrations/20220802162238-create-spot.js
+++ backend/db/migrations/20220802162238-create-spot.js
@@ -3,6 +3,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Spots', {
       id: {
+        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
--- backend/db/migrations/20220802162559-create-review.js
+++ backend/db/migrations/20220802162559-create-review.js
@@ -3,6 +3,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Reviews', {
       id: {
+        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
