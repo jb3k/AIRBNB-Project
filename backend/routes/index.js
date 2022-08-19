e4f2fba3b3fef8f60dfc53bbbcd00204f2a0ce55
JB Kam <jb3.kam@gmail.com>
2022-08-19 14:25:22 -0700

    updated the backend to deploy to heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 // router.get('/hello/world', function (req, res) {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
 //     res.send('Hello World!');
 // });
 
-const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
@@ -34,19 +34,23 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
+//new
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        res.status(201).json({});
+        // return res.json({});
     });
 }
 
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    });
-});
+//old (pre-frontend)
+// router.get("/api/csrf/restore", (req, res) => {
+//     const csrfToken = req.csrfToken();
+//     res.cookie("XSRF-TOKEN", csrfToken);
+//     res.status(200).json({
+//         'XSRF-Token': csrfToken
+//     });
+// });
 
 module.exports = router;
