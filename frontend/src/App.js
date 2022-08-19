6099266a6d67bee26bb304cd548401fc2300a415
JB Kam <jb3.kam@gmail.com>
2022-08-19 14:20:41 -0700

    added the bonus modal feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./component/LoginFormPage/index";
 import SignupFormPage from "./component/SignupFormPage/index"
 import * as sessionActions from "./store/session";
 import Navigation from "./component/Navigation/index"
@@ -19,9 +18,6 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
