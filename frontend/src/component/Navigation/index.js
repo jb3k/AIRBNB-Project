d6754950eca56fa83d244f8260644e6a5d1e0f84
JB Kam <jb3.kam@gmail.com>
2022-08-27 15:35:38 -0700

    finished styling the menu drop down bar

--- frontend/src/component/Navigation/index.js
+++ frontend/src/component/Navigation/index.js
@@ -12,7 +12,6 @@ import * as sessionActions from '../../store/session';
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
   const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
   const [open, setOpen] = useState(false)
 
 
@@ -32,8 +31,8 @@ function Navigation({ isLoaded }) {
             <LoginFormModal />
           </div>
           <div className='dropdown-signup'>
-            <NavLink to="/signup"
-              style={{ textDecoration: 'none', color: 'black' }} >
+            <NavLink to="/signup" className={'dropdown-signup'}
+              style={{ textDecoration: 'none' }} >
               Sign up
             </NavLink>
           </div>
@@ -130,41 +129,12 @@ function Navigation({ isLoaded }) {
             style={{ textDecoration: 'none' }}>
             <i class="fa-solid fa-handshake"> </i> | FairBnB
           </NavLink>
-
         </div>
         <div className='become-host'>
-          {/* <NavLink to='/spots/current'> */}
           <button className='host-button' onClick={loginAlert}>{wordSwitcher()}</button>
-          {/* </NavLink> */}
         </div>
 
         {dropdownMenu()}
-        {/* <div>
-          <label className='search-bar'>
-            <input
-              className='bar'
-              type="text"
-              placeholder="Anywhere | Any week | Add guests "
-            />
-            <i class="fa fa-search" id='search-icon'></i>
-          </label>
-        </div> */}
-
-        {/* <div>
-          <div className='dropdown'>
-            <button className='menu-bttn' onClick={() => setOpen(!open)}>
-              <div className='login-icon'>
-                <i class="fa fa-bars fa-lg"></i>
-              </div>
-              <div className={'signup-icon'}>
-                <i class="fa-solid fa-user"> </i>
-              </div>
-            </button>
-          </div> */}
-
-
-        {/* </div> */}
-
 
       </div>
     </div>
